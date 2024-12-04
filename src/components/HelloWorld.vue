ef4737c904f9e921205044a9ce99baa2e3c9e33f
Pine Tree <tygh.soon@foxmail.com>
2024-12-04 22:42:29 +0800

    个人开发分支提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,7 @@ defineProps<{
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
+      新功能1
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
