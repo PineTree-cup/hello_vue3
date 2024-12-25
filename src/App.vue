a1155872689b4a7eaf25e146e43ad60d66fd1726
Pine Tree <tygh.soon@foxmail.com>
2024-12-25 21:50:17 +0800

    引入路由

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,67 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="app">
+    <h2 class="title">Vue路由测试</h2>
+    <!-- 导航区 -->
+    <div class="navigate">
+      <RouterLink to="/home" active-class="activeClass">首页</RouterLink>
+      <RouterLink to="/news" active-class="activeClass">新闻</RouterLink>
+      <RouterLink to="/about" active-class="activeClass">关于</RouterLink>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <!-- 展示区 -->
+    <div class="main-content">
+      <RouterView></RouterView>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts" setup name="App">
+import {RouterView,RouterLink} from 'vue-router'
 
-.logo {
+</script>
+
+<style>
+/* App */
+.title {
+  text-align: center;
+  word-spacing: 5px;
+  margin: 30px 0;
+  height: 70px;
+  line-height: 70px;
+  background-image: linear-gradient(45deg, gray, white);
+  border-radius: 10px;
+  box-shadow: 0 0 2px;
+  font-size: 30px;
+}
+.navigate {
+  display: flex;
+  justify-content: space-around;
+  margin: 0 100px;
+}
+.navigate a {
   display: block;
-  margin: 0 auto 2rem;
+  text-align: center;
+  width: 90px;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 10px;
+  background-color: gray;
+  text-decoration: none;
+  color: white;
+  font-size: 18px;
+  letter-spacing: 5px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.navigate a.activeClass {
+  background-color: #64967E;
+  color: #ffc268;
+  font-weight: 900;
+  text-shadow: 0 0 1px black;
+  font-family: 微软雅黑;
+}
+.main-content {
+  margin: 30px auto 0;
+  border-radius: 10px;
+  width: 90%;
+  height: 400px;
+  border: 1px solid;
 }
 </style>
