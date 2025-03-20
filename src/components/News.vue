959758de2dbf2d49c42685cf336a5aa88b8665c2
Pine Tree <tygh.soon@foxmail.com>
2025-03-20 23:56:22 +0800

    vue使用element-ui自定义样式思路分享【实操】

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="news">
-    <ul>
-      <li><a href="#">新闻001</a></li>
-      <li><a href="#">新闻002</a></li>
-      <li><a href="#">新闻003</a></li>
-      <li><a href="#">新闻004</a></li>
-    </ul>
-    <button @click="getNews">获取用户信息</button>
+  <div class="new-tabs">
+    <el-tabs
+        v-model="editableTabsValue"
+        type="card"
+        class="demo-tabs"
+        editable
+        @edit="handleTabsEdit"
+    >
+      <template #add-icon>
+        <el-icon><Select/></el-icon>
+      </template>
+      <el-tab-pane
+          v-for="item in editableTabs"
+          :key="item.name"
+          :label="item.title"
+          :name="item.name"
+      >
+        {{ item.content }}
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script setup lang="ts" name="News">
 import axios from "axios";
+import { ref } from 'vue'
+import { Select } from '@element-plus/icons-vue'
+import type { TabPaneName } from 'element-plus'
+let tabIndex = 2
+const editableTabsValue = ref('2')
+const editableTabs = ref([
+  {
+    title: 'Tab 1',
+    name: '1',
+    content: 'Tab 1 news content',
+  },
+  {
+    title: 'Tab 2',
+    name: '2',
+    content: 'Tab 2 news content',
+  },
+])
 
+const handleTabsEdit = (
+    targetName: TabPaneName | undefined,
+    action: 'remove' | 'add'
+) => {
+  if (action === 'add') {
+    const newTabName = `${++tabIndex}`
+    editableTabs.value.push({
+      title: 'New Tab',
+      name: newTabName,
+      content: 'New Tab content',
+    })
+    editableTabsValue.value = newTabName
+  } else if (action === 'remove') {
+    const tabs = editableTabs.value
+    let activeName = editableTabsValue.value
+    if (activeName === targetName) {
+      tabs.forEach((tab, index) => {
+        if (tab.name === targetName) {
+          const nextTab = tabs[index + 1] || tabs[index - 1]
+          if (nextTab) {
+            activeName = nextTab.name
+          }
+        }
+      })
+    }
+
+    editableTabsValue.value = activeName
+    editableTabs.value = tabs.filter((tab) => tab.name !== targetName)
+  }
+}
 function getNews() {
   axios.get('http://localhost:8888/list').then(
       response => {
@@ -28,6 +87,12 @@ function getNews() {
 
 <style scoped>
 /* 新闻 */
+.new-tabs :deep(.el-tabs__new-tab){
+  height: 30px;
+  width: 30px;
+  border-radius: 15px;
+  font-size: 16px;
+}
 .news {
   padding: 0 20px;
   display: flex;
